04207511212eb6e3d12aaa8c99d4bdf9187b9a1b
kumatabetai <90751879+kumatabetai@users.noreply.github.com>
2021-10-23 01:41:47 +0900

    ユーザー管理画面修正２

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :age
+  belongs_to :sex
+  belongs_to :profession
 end
