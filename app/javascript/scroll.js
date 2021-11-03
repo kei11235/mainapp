47f168f26525b85d62df6d41ef09cf8f34849598
kumatabetai <90751879+kumatabetai@users.noreply.github.com>
2021-11-03 11:54:40 +0900

    コメント機能完成

--- app/javascript/scroll.js
+++ app/javascript/scroll.js
@@ -4,6 +4,13 @@ function scroll() {
       scrollend.scrollTop = scrollend.scrollHeight;
   }
   scrollToEnd()
+
+  const scrollSubmit = document.getElementById("scroll-submit")
+  scrollSubmit.addEventListener("click", function() {
+    window.setTimeout(function(){
+      scrollToEnd()
+  }, 200);
+  });
 }
 
 window.addEventListener('load',scroll)
